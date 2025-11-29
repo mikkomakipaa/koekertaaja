5e0d0f459fdf870f245b1586e3365697106d5b91
Mikko Mäkipää <mikko.makipaa@pm.me>
2025-11-29 16:46:26 +0200

    🔖 Checkpoint: Edit /Users/mikko.makipaa/koekertaaja/src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Footer } from '@/components/shared/Footer';
 import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
